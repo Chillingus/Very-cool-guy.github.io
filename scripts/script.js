932d7d3a774f80d4a79c3cf11ed6ccdc3847c106
cracktorio <mlg.nlgamer@gmail.com>
2026-05-22 22:02:05 +0200

    cookies for the cookie monster

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
-var enabled = true
+var enabled = !document.cookie.includes('barrelroll=disabled')
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('barrelroll')
+    if (btn) {
+        if (enabled) {
+            document.body.classList.add("barrelroll")
+            btn.innerText = "DISABLE BARREL ROLL"
+        } else {
+            document.body.classList.remove("barrelroll")
+            btn.innerText = "ENABLE BARREL ROLL :3"
+        }
+    }
+})
+
 function leon() {
     const leon_response = document.createElement("p");
     leon_response.innerHTML = "まだ何もなかった時、神は天と地を造りました. 地は形も定まらず、闇に包まれた水の上を、さらに神の霊が覆っていました. <br>「光よ、輝き出よ.」神が言われると、光がさっとさしてきました.";
@@ -7,6 +21,7 @@ function leon() {
 
 function barrelrolltoggel(){
     var a = barrelroll
+    document.cookie = "barrelroll=" + (enabled ? "disabled" : "enabled") + "; path=/; max-age=31536000";
 
     if(enabled){
         a.innerText = "ENABLE BARREL ROLL :3"
